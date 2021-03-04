55458823c98fe36ed66270c984caabf15c092f3b
StanDarius24 <standarius24@gmail.com>
2021-03-04 23:14:05 +0200

    Mare creatie + Bogdannis

--- disc.c
+++ disc.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <unistd.h>
-       #include <fcntl.h>
-
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include<string.h>
+#include <dirent.h>
+#include <limits.h>
 #include "disc.h"
+
+#include "structura.h"
 void checkdir()
 {
 	struct stat info;
@@ -34,7 +38,93 @@ void checkdir()
 		}
 
 }
-int main()
-{	checkdir();
-	return 0;
+
+void newUser(char *Name, struct ClientData * DataClient,int i)
+{	int sw=1;
+	DIR *director,*director2;
+	struct dirent *dir,*dir2;
+	struct stat info,info2;
+	char *name = malloc(sizeof(char)*30);
+	char *path = malloc(sizeof(char)*50);
+	if(!(director=opendir("server")))
+	{
+		printf("Eroare la deschiderea directorului server\n");
+		exit(9);
+	}
+	while((dir=readdir(director))>0 && sw)
+	{
+		strcpy(name,dir->d_name);
+			if(strcmp(name, ".") == 0 || strcmp(name, "..") == 0 )
+					continue;
+		snprintf(path,49,"server/%s",name);
+	
+		if(lstat(path,&info)<0)
+		{
+			printf("Eroare la functia stat\n");
+			exit(10);
+		}
+
+		if(S_ISDIR(info.st_mode) && (strcmp(Name,name)==0))
+		{
+			printf("director existent, utilizator vechi\n");
+			sw=0;
+			if(!(director2=opendir(path)))
+			{
+				printf("Eroare la deschiderea directorului 2\n");
+				exit(12);
+			}
+			char *names=malloc(sizeof(char)*50);
+			char *path2=malloc(sizeof(char)*50);
+			while((dir2=readdir(director2))>0)
+			{
+				strcpy(names,dir2->d_name);
+				if(strcmp(names, ".") == 0 || strcmp(names, "..") == 0 )
+					continue;
+				snprintf(path2,49,"%s/%s",path,names);
+				printf("path to files : %s \n",path2);
+
+				if(lstat(path2,&info2)<0)
+				{
+					printf("Eroare la lstat in dir 2\n");
+					exit(13);
+				}
+				if(S_ISREG(info2.st_mode))
+				{
+					DataClient[i].Fisier=AddFile(DataClient[i].Fisier,Name);
+				}
+
+
+			}
+			closedir(director2);
+			free(names);
+			free(path2);
+
+		}
+		
+		if(sw)
+		{
+			printf("Utilizator nou, generam director!\n");
+
+			pid_t child;
+
+			if((child=fork())<0)
+			{
+				printf("Eroare la proces xD\n");
+				exit(112);
+			}else
+			if(child==0)
+			{
+				char *milsugio=malloc(sizeof(char)*112);
+				snprintf(milsugio,100,"server/%s",Name);
+				execlp("mkdir","mkdir",milsugio,NULL);
+			}
+
+
+		}
+		
+
+	}
+	closedir(director);
+	free(name);
+	free(path);
 }

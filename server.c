855d20f092bd0aebb89c5117e13c81cfc09aa18c
StanDarius24 <standarius24@gmail.com>
2021-03-05 22:04:03 +0200

    Problems solved, am factorizat functia newclient in server.c pentru a putea modifica datele din DataClient. Mai ramane partea de partajare de fisiere :D

--- server.c
+++ server.c
@@ -9,6 +9,8 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<string.h>
+#include <dirent.h>
+#include <limits.h>
 #include "structura.h"
 #include "disc.h"
 #define MAX_SIZE 10
@@ -63,17 +65,18 @@ void commands(int i,char *Name)
 			memset(&file,0x00,sizeof(file));
 			recv(ClientSocket[i],&file,sizeof(file),0);
 			printf("file= %s\n",file);
+			DataClient[i]=*AdaugareFisier(&DataClient[i],file);
 			if(lstat(file,&info)<0)
 			{
 			printf("Eroare la functia stat %s\n",file);
 			exit(99);
 			}
-			else
+				else
 				if(S_ISREG(info.st_mode))
 				{
 					int fd1,fd2;
 					int n;
-					char *buff=malloc(sizeof(char)*1024);
+					char *buff=malloc(sizeof(char)*1025);
 					if((fd1=open(file,O_RDONLY))<0)
 					{
 						printf("Eroare la deschidere fisier\n");
@@ -92,7 +95,7 @@ void commands(int i,char *Name)
 
 						while((n=read(fd1,buff,1024))>0)
 						{
-							if(write(fd2,buff,strlen(buff))<0)
+							if(write(fd2,buff,n)<0)
 							{
 								printf("Eroare la scrierea in fisier\n");
 								exit(24);
@@ -112,6 +115,97 @@ void commands(int i,char *Name)
 	}
 
 }
+void newUser(char *Name, struct ClientData * DataClient,int i)
+{	int sw=1;
+	DIR *director,*director2;
+	struct dirent *dir,*dir2;
+	struct stat info,info2;
+	char *name = malloc(sizeof(char)*30);
+	char *path = malloc(sizeof(char)*50);
+	if(!(director=opendir("server")))
+	{
+		printf("Eroare la deschiderea directorului server\n");
+		exit(9);
+	}
+	while((dir=readdir(director))>0 && sw)
+	{
+		strcpy(name,dir->d_name);
+			if(strcmp(name, ".") == 0 || strcmp(name, "..") == 0 )
+					continue;
+		snprintf(path,49,"server/%s",name);
+	
+		if(lstat(path,&info)<0)
+		{
+			printf("Eroare la functia stat\n");
+			exit(10);
+		}
+
+		if(S_ISDIR(info.st_mode) && (strcmp(Name,name)==0))
+		{
+			printf("director existent, utilizator vechi\n");
+			sw=0;
+			if(!(director2=opendir(path)))
+			{
+				printf("Eroare la deschiderea directorului 2\n");
+				exit(12);
+			}
+			char *names=malloc(sizeof(char)*50);
+			char *path2=malloc(sizeof(char)*50);
+			while((dir2=readdir(director2))>0)
+			{
+				strcpy(names,dir2->d_name);
+				if(strcmp(names, ".") == 0 || strcmp(names, "..") == 0 )
+					continue;
+				snprintf(path2,49,"%s/%s",path,names);
+				printf("path to files : %s \n",path2);
+				DataClient[i]=*AdaugareFisier(&DataClient[i],names);
+				
+				if(lstat(path2,&info2)<0)
+				{
+					printf("Eroare la lstat in dir 2\n");
+					exit(13);
+				}
+				if(S_ISREG(info2.st_mode))
+				{
+						printf("Adaugare \n");
+					DataClient[i]=*AdaugareFisier(&DataClient[i],names);
+				}
+
+
+			}
+			closedir(director2);
+			free(names);
+			free(path2);
+
+		}
+		
+		if(sw==1)
+		{
+			printf("Utilizator nou, generam director!\n");
+
+			pid_t child;
+
+			if((child=fork())<0)
+			{
+				printf("Eroare la proces xD\n");
+				exit(112);
+			}else
+			if(child==0)
+			{
+				char *xD=malloc(sizeof(char)*112);
+				snprintf(xD,100,"server/%s",Name);
+				execlp("mkdir","mkdir",xD,NULL);
+			}
+
+
+		}
+		
+
+	}
+	closedir(director);
+	free(name);
+	free(path);
+}
 
 void * threadClient( void *arg )
 {
@@ -120,8 +214,8 @@ void * threadClient( void *arg )
 	printf("i=%d\n",i);
 	sendMessage("Connected!",i);
 	strcpy(Name,receiveMessage(i));
-	newUser(Name,DataClient,i);
 	DataClient[i]=*CreereClient(Name);
+	newUser(Name,DataClient,i);
 	PrintareClient(&DataClient[i]);
 	commands(i,Name);
 	printf("%s has disconnected!\n",Name);

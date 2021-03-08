b0e873b90d91366f365519df75993e79ffbbb0ba
StanDarius24 <standarius24@gmail.com>
2021-03-08 21:22:15 +0200

    Mare plot twist, ne-am luat tzeaca de la thread-uri ne-au incarcat ca pe gascani, dar le-am venit de hac cu bogdannis.

--- structura.h
+++ structura.h
@@ -1,21 +1,24 @@
 #ifndef STRUCTURA_H
 #define STRUCTURA_H
+#include <sys/socket.h>	
 typedef struct node{
     char NumeFisier[25];
     struct node *urm;
-}node;
+}node;	
+
 typedef struct ClientData{
-    char Nume[25];
+	struct sockaddr_in *address;
+	int sockfd;
+	int uid;
+    char Nume[32];
     struct node *Fisier;    
 }ClientData;				
 							
-void parcurgereFisiere(node *Nod);
-node * CreeateNode(char *fisier);
+node *CreeateNode(char *fisier);
 node * AddFile(node *Nod,char *filename);
-ClientData *CreereClient(char *name);
-ClientData *AdaugareFisier(ClientData *Client,char *numeFisier);
-void PrintareClient(ClientData *Client);
+void parcurgereFisiere(node *Nod);
 void PrintareAllFiles(node *allFiles);
 int FileAlreadyExistsInAllFiles(node *allFiles, char *filename);
-node * AddToAllFiles(node *allFiles, char *filename);
+node *AddToAllFiles(node *allFiles, char *filename);
+void checkdir();
 #endif
